f7b5fece8c2ace51d1b59d2d3c73eec051535f13
David Baba <davidbabasalifu2020@gmail.com>
2021-11-24 20:49:06 +0100

    My project structure setup for davablog api

---
+++ src/main/java/com/davablog/davablog/DavesBlogApplication.java
@@ -0,0 +1,13 @@
+package com.davablog.davablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavesBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavesBlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/davablog/davablog/service/UserService.java
@@ -0,0 +1,4 @@
+package com.davablog.davablog.service;
+
+public class UserService {
+}
