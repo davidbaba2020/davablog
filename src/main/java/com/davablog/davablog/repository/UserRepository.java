3b9ae96c0c10b23d523da79a7d026210f74552f5
David Baba <davidbabasalifu2020@gmail.com>
2021-11-24 21:43:08 +0100

    Created my Post repository

---
+++ src/main/java/com/davablog/davablog/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.davablog.davablog.repository;
+
+import com.davablog.davablog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Post, Long> {
+    //
+}
